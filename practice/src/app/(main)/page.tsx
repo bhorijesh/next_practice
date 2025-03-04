0067a36e81a1c97a17d5aadd78f416961bc9a3b1
bhorijesh <rijeshbhochhibhoya@gmail.com>
2025-03-04 17:38:17 +0545

    update Practice

--- practice/src/app/(main)/page.tsx
+++ practice/src/app/(main)/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
-
-
 export default function home(){
     return(
         <>
-        <h1>Welocome To HomePage!</h1>
-        </>
-    ) 
+        <h1>Welcome To HomePage!</h1>
+        <Link href="/articles/breaking-news-123?lang=en">
+          Read in English
+        </Link>
+        <Link href="/articles/breaking-news-123?lang=fr">
+          Read in French
+        </Link>
+      </>
+    );
 }
