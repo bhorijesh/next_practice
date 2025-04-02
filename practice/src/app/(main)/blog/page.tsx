4d338107a04d7229fbc16c23ad6904d48a37143c
bhorijesh <rijeshbhochhibhoya@gmail.com>
2025-04-02 13:52:29 +0545

    Notion Clone

--- practice/src/app/(main)/blog/page.tsx
+++ practice/src/app/(main)/blog/page.tsx
@@ -2,6 +2,11 @@ import { Metadata } from "next";
 export const metadata : Metadata ={
     title : "Blog",
 };
-export default function Blog(){
+export default async function Blog(){
+    await new Promise(resolve =>{
+        setTimeout(()=>{
+            resolve("intentaional delay");
+        }, 2000)
+    })
     return <h1>My Blog</h1>
 }
--- practice/src/app/(main)/product/[productid]/reviews/[reviewid]/page.tsx
+++ practice/src/app/(main)/product/[productid]/reviews/[reviewid]/page.tsx
@@ -1,10 +1,18 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number){
+    return Math.floor(Math.random()* count);
+}
+
 export default async function ProductReview({
     params,
 }:{
     params :Promise<{productid : string; reviewid : string}>;
 }){
+    const random = getRandomInt(2);
+    if(random==1){
+        throw new Error("Error Loading Review")
+    }
     const {productid , reviewid} = await params
     if (parseInt(reviewid)>100){
         notFound();
